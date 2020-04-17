604d12e198b608d6a828f084fe42144d86fde1a8
MobileVortex <mobilevortex9@gmail.com>
2020-04-16 21:07:30 -0700

    Still fixing copying dotfiles

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@
 	## Kyle's Arch Budgie Desktop Installation and  dotfile script ##
 	## dot files were stolen and tweaked from LARBS (Luke Smith)   ##
 
+	## Makes ~/.config and moves dotfiles to the home directory ##
+	mkdir ~/.config
+	cp .xinitrc ~
+	cp .profile ~
+	cp .zprofile ~
+	cp .zshrc ~
+	cp aliasrc ~/.config/
+	cp -r nvim ~/.config/
+
 	## Makes pacman and yay have purtay coolers ##
 	sudo bash candy.sh
 
@@ -16,9 +25,6 @@
 	tar -xvf yay.tar.gz &&
 	cd "yay" &&
 	makepkg --noconfirm -si
-
-	## Move dotfiles to home ##
-	bash copy.sh
 
 	## Installs packages with yay, seemed to work with one line so there are 4 - and it's easier to read! ##
 	yay --noconfirm --needed -S gparted adapta-gtk-theme papirus-icon-theme budgie-desktop budgie-extras dash f2fs-tools abiword
