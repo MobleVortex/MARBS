2e7160605e8ee3d00cc7bb573b2d95463bb03624
MobleVortex <mobilevortex9@gmail.com>
2023-11-10 13:16:29 -0800

    Update candy.sh

--- candy.sh
+++ candy.sh
@@ -2,5 +2,7 @@
 # Run this script as root
 
 	grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/Color/" /etc/pacman.conf &&
+	grep "^VerbosePkgLists" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/VerbosePkgLists/" /etc/pacman.conf &&
+	grep "^ParallelDownloads = 5" /etc/pacman.conf >/dev/null || sed -i "s/^#Color$/ParallelDownloads = 5/" /etc/pacman.conf && 
 	grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 
