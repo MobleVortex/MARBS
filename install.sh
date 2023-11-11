1ec758052c5093878c5441c529569ba07f7c165a
MobleVortex <mobilevortex9@gmail.com>
2023-11-10 17:47:39 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,18 +20,22 @@
 	cd "yay" &&
 	makepkg --noconfirm -si
 
- 	## Installs reflector and sort mirrors for the fast downloads ##
-#	yay --noconfirm --needed -S reflector rsync
+ 	## Installs reflector and sort mirrors for the fast downloads 
+#	yay --noconfirm --needed -S reflector rsync 
 
+	## No longer necessary with new archinstall script
 #	sudo reflector --verbose --country 'United States' --sort rate --save /etc/pacman.d/mirrorlist
 
 	## Installs packages with yay, didn't seem to work with one line so there are 4 - and it's easier to read! ##
 	yay --noconfirm --needed -S reflector rsync gparted adapta-gtk-theme papirus-icon-theme nano-syntax-highlighting f2fs-tools abiword zsh foot mpv rhythmbox breeze-adapta-cursor-theme-git dashbinsh brave-bin unzip zsh-syntax-highlighting ttf-liberation librewolf-bin noto-fonts ttf-roboto dconf-editor gnome-browser-connector gnome-bluetooth gnome-screenshot
 
-	# Remove unwanted gnome packages
+	## Remove unwanted gnome packages
 
-#	yay --noconfirm --needed -R 
+	yay --noconfirm --needed -R gnome-tour gnome-logs gnome-maps gnome-software gnome-music gnome-clocks gnome-contacts gnome-calendar gnome-user-docs gnome-remote-desktop gnome-characters gnome-user-share gnome-text-editor gnome-connections gnome-font-viewer yelp malcontent totem orca simple-scan evince epiphany rygel
 
+	## Load dconf settings for gnome
+ 	dconf load / < dconf-settings.ini
+	
  	## change shell to zsh & source
   	chsh -s /bin/zsh
 	source ~/.zshrc
