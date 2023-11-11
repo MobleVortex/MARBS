19d21079248be19702a78aae9a1900d2d9bb9f76
MobleVortex <mobilevortex9@gmail.com>
2023-11-10 17:37:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@
 #	sudo reflector --verbose --country 'United States' --sort rate --save /etc/pacman.d/mirrorlist
 
 	## Installs packages with yay, didn't seem to work with one line so there are 4 - and it's easier to read! ##
-	yay --noconfirm --needed -S reflector rsync gparted adapta-gtk-theme papirus-icon-theme nano-syntax-highlighting f2fs-tools abiword zsh foot mpv rhythmbox breeze-adapta-cursor-theme-git dashbinsh brave-bin unzip zsh-syntax-highlighting ttf-liberation librewolf-bin noto-fonts ttf-roboto dconf-editor gnome-browser-connector gnome-bluetooth
+	yay --noconfirm --needed -S reflector rsync gparted adapta-gtk-theme papirus-icon-theme nano-syntax-highlighting f2fs-tools abiword zsh foot mpv rhythmbox breeze-adapta-cursor-theme-git dashbinsh brave-bin unzip zsh-syntax-highlighting ttf-liberation librewolf-bin noto-fonts ttf-roboto dconf-editor gnome-browser-connector gnome-bluetooth gnome-screenshot
 
 	# Remove unwanted gnome packages
 
